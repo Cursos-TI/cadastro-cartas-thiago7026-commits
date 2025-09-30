928768fd0557de36ce846a4f52536630f5f592e7
thiago7026-commits <thiago.7026@gmail.com>
2025-09-30 18:02:53 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main(){
-  char
+  
+  // Area para definição das variaveis para armazenar as propriedades da cidade 
+  
+    char Estado: A;
+    char Codigo da Carta: A01[50];
+    char Nome da Cidade: Anori;
+    int População: 17.194 ;
+    int Area (em km²):  5.795,283 km²;
+    float PIB: R$ 43,7 bilhões;
+    int Numero de pontos turisticos:3
+
+      printf
+    
+  
+    
 
 
 
